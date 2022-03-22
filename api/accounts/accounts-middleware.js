fc66fa75082bbd3a553772e069a3c2e27c82d8fa
XavierTwitty <x.twitty@gmail.com>
2022-03-22 09:03:09 -0400

    update

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -9,7 +9,7 @@ exports.checkAccountPayload = (req, res, next) => {
    res.status(400).json({ message: "name of account must be between 3 and 100" })
   } else if (typeof req.body.budget !== "number") {
     res.status(400).json({ message: "budget of account must be a number" })
-  } else if (req.body.budget < 0 || req.body.budget > 1000000) {
+  } else if (req.body.budget <= 0 || req.body.budget >= 1000000) {
     res.status(400).json({ message: "budget of account is too large or too small" }
     )
   } else {
